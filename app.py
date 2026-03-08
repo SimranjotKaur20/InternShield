4116cf388d6b008052058eb23e0d685df394aed4
Simranjot Kaur <simigrewal317@gmail.com>
2026-03-08 11:55:55 +0530

    Add files via upload
    changed dashboard to home

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("dashboard.html")
+    return render_template("home.html")
 
 @app.route("/scanner")
 def scanner():
